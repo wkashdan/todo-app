d678cf3d9a332ec0842993333e25232a154eb6bf
Will Kashdan <wgkashdan@gmail.com>
2016-10-21 15:53:17 -0400

    Added Item and List Model

---
+++ scripts/Item.js
@@ -0,0 +1,7 @@
+export class Item {
+	constructor(name) {
+		this.name = name;
+		this.isComplete = false;
+		this.isDeleted = false;
+	}
+}
---
+++ scripts/List.js
@@ -0,0 +1,7 @@
+export class List {
+	constructor(name) {
+		this.name = name;
+		this.items = [];
+		this.isDeleted = false;
+	}
+}
